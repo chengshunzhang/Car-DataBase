a59fac9f338139477d753f78a9a4c0a6c2621ec6
chengshunzhang <chengshun.zhang@outlook.com>
2018-12-30 15:54:31 -0800

    add login and authentication filter

---
+++ src/main/java/com/example/cardatabase/AuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.example.cardatabase;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+import com.example.cardatabase.service.AuthenticationService;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthenticationFilter extends GenericFilterBean {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain filterChain) throws IOException, ServletException {
+		Authentication authentication = 
+				AuthenticationService.getAuthentication((HttpServletRequest)request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		filterChain.doFilter(request, response);
+	}
+}
--- src/main/java/com/example/cardatabase/SecurityConfig.java
+++ src/main/java/com/example/cardatabase/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.cardatabase;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.
 authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.cardatabase.service.UserDetailServiceImpl;
 
@@ -32,7 +34,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
+		http.cors().and().authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/login").permitAll()
+		.anyRequest().authenticated().and()
+		// filter for api/login requests
+		.addFilterBefore(new LoginFilter("/login", authenticationManager()),
+				UsernamePasswordAuthenticationFilter.class)
+		// filter for other requests to check JWT in header
+		.addFilterBefore(new AuthenticationFilter(),
+				UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	
