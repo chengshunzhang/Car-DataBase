7914ae3486475681d2f42000d6efbefb92dcb79e
chengshunzhang <chengshun.zhang@outlook.com>
2018-12-30 11:31:17 -0800

    save users to database

---
+++ src/main/java/com/example/cardatabase/service/UserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.cardatabase.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.cardatabase.domain.User;
+import com.example.cardatabase.domain.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User currentUser = repository.findByUsername(username);
+		UserDetails user = new org.springframework.security.core.
+				userdetails.User(username, currentUser.getPassword(), 
+						true, true, true, true,
+						AuthorityUtils.createAuthorityList(currentUser.getRole()));
+		return user;
+	}
+}
